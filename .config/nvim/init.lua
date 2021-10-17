88e75c4ccd2c5afd8cb14498f9f4027d9e53f82e
lwsn <lowe.stalnacke@gmail.com>
2021-10-17 15:08:21 +0200

    Reverts config split

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,15 +26,6 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Highlight on yank
-vim.api.nvim_exec(
-   'augroup YankHighlight\n'
-      .. 'autocmd!\n'
-      .. 'autocmd TextYankPost * silent! lua vim.highlight.on_yank {timeout = 500}\n'
-      .. 'augroup end',
-   false
-)
-
 --------------
 -- Mappings --
 --------------
@@ -85,6 +76,7 @@ vim.api.nvim_set_keymap('', '<LEADER>q', ':q<CR>', { noremap = true })
 -- Auto install plugin manager
 
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
    packer_bootstrap = vim.fn.system {
@@ -210,6 +202,14 @@ return require('packer').startup {
       }
 
       use {
+         'machakann/vim-highlightedyank',
+         config = function()
+            -- built in yank highlight crashes when deleting more than 430 lines when LSP is enabled?????
+            vim.g.highlightedyank_highlight_duration = 500
+         end,
+      }
+
+      use {
          'APZelos/blamer.nvim',
          config = function()
             vim.g.blamer_delay = 500
@@ -224,6 +224,43 @@ return require('packer').startup {
             require('lualine').setup {
                options = {
                   theme = 'onedark',
+                  section_separators = { '', '' },
+                  component_separators = { '', '' },
+               },
+               sections = {
+                  lualine_a = {
+                     {
+                        'mode',
+                        format = function(str)
+                           return (
+                                 ({
+                                    ['V-BLOCK'] = 'B',
+                                    ['V-LINE'] = 'L',
+                                 })[str] or str:sub(0, 1)
+                              )
+                        end,
+                     },
+                  },
+                  lualine_c = {
+                     {
+                        'filename',
+                        path = 1,
+                        format = function(str)
+                           return str:gsub('/./', '/')
+                        end,
+                     },
+                  },
+               },
+               inactive_sections = {
+                  lualine_c = {
+                     {
+                        'filename',
+                        path = 1,
+                        format = function(str)
+                           return str:gsub('/./', '/')
+                        end,
+                     },
+                  },
                },
             }
          end,
@@ -240,13 +277,103 @@ return require('packer').startup {
             'vijaymarupudi/nvim-fzf',
             'kyazdani42/nvim-web-devicons',
          },
-         config = require('plugin-config.fzf-config').config,
+         config = function()
+            require('fzf-lua').setup {
+               winopts = {
+                  win_height = 0.7,
+                  win_width = 0.8,
+                  win_border = true,
+               },
+               fzf_opts = {
+                  ['--layout'] = false,
+               },
+               files = {
+                  cmd = "rg --files --hidden -g '!{.git,node_modules}/*'",
+               },
+            }
+
+            require('fzf-lua').all_lines = function()
+               require('fzf-lua').files {
+                  cmd = 'rg --hidden --line-number '
+                     .. '--no-heading --vimgrep '
+                     .. '--smart-case --trim '
+                     .. "-g '!{.git,node_modules}/*' '^.*[A-z]+.*'",
+               }
+            end
+
+            vim.api.nvim_set_keymap(
+               'n',
+               '<LEADER><TAB>',
+               "<cmd>lua require('fzf-lua').buffers()<CR>",
+               { noremap = true }
+            )
+            vim.api.nvim_set_keymap(
+               'n',
+               '<LEADER><Space>',
+               "<cmd>lua require('fzf-lua').files()<CR>",
+               { noremap = true }
+            )
+            vim.api.nvim_set_keymap(
+               'n',
+               '<LEADER>r',
+               "<cmd>lua require('fzf-lua').oldfiles()<CR>",
+               { noremap = true }
+            )
+            vim.api.nvim_set_keymap(
+               'n',
+               '<LEADER>s',
+               [[<cmd>lua require('fzf-lua').all_lines()<CR>]],
+               { noremap = true }
+            )
+         end,
       }
 
       use {
          'mhartington/formatter.nvim',
          run = 'npm install -g @fsouza/prettierd',
-         config = require('plugin-config.formatter-config').config,
+         config = function()
+            local prettierd = function()
+               return {
+                  exe = 'prettierd',
+                  args = { vim.api.nvim_buf_get_name(0) },
+                  stdin = true,
+               }
+            end
+
+            require('formatter').setup {
+               logging = true,
+               filetype = {
+                  javascript = { prettierd },
+                  typescript = { prettierd },
+                  typescriptreact = { prettierd },
+                  json = { prettierd },
+                  lua = {
+                     function()
+                        return {
+                           exe = 'stylua',
+                           args = {
+                              '--config-path '
+                                 .. os.getenv 'XDG_CONFIG_HOME'
+                                 .. '/stylua/stylua.toml',
+                              '-',
+                           },
+                           stdin = true,
+                        }
+                     end,
+                  },
+               },
+            }
+
+            vim.api.nvim_exec(
+               [[
+            augroup FormatAutogroup
+            autocmd!
+            autocmd BufWritePost *.tsx,*.ts,*.js,*.jsx,*.json,*.lua FormatWrite
+            augroup END
+            ]],
+               true
+            )
+         end,
       }
 
       -- LSP
@@ -259,8 +386,158 @@ return require('packer').startup {
             'saadparwaiz1/cmp_luasnip',
             'L3MON4D3/LuaSnip',
          },
-         run = require('plugin-config.lsp-config').run,
-         config = require('plugin-config.lsp-config').config,
+         run = function()
+            local required_servers = {
+               'pylsp',
+               'pyright',
+               'sumneko_lua',
+               'tsserver',
+               'yamlls',
+               'html',
+            }
+
+            for _, server in pairs(required_servers) do
+               require('nvim-lsp-installer').install(server)
+            end
+         end,
+         config = function()
+            local lsp_installer = require 'nvim-lsp-installer'
+            local cmp = require 'cmp'
+            local luasnip = require 'luasnip'
+
+            local has_words_before = function()
+               local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+               return col ~= 0
+                  and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+                        :sub(col, col)
+                        :match '%s'
+                     == nil
+            end
+
+            cmp.setup {
+               snippet = {
+                  expand = function(args)
+                     luasnip.lsp_expand(args.body)
+                  end,
+               },
+               mapping = {
+                  ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-e>'] = cmp.mapping.close(),
+                  ['<CR>'] = cmp.mapping.confirm {
+                     behavior = cmp.ConfirmBehavior.Replace,
+                     select = true,
+                  },
+                  ['<Tab>'] = cmp.mapping(function(fallback)
+                     if cmp.visible() then
+                        cmp.select_next_item()
+                     elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                     elseif has_words_before() then
+                        cmp.complete()
+                     else
+                        fallback()
+                     end
+                  end, {
+                     'i',
+                     's',
+                  }),
+
+                  ['<S-Tab>'] = cmp.mapping(function(fallback)
+                     if cmp.visible() then
+                        cmp.select_prev_item()
+                     elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                     else
+                        fallback()
+                     end
+                  end, {
+                     'i',
+                     's',
+                  }),
+               },
+               sources = {
+                  { name = 'nvim_lsp' },
+                  { name = 'luasnip' },
+               },
+            }
+
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+            lsp_installer.on_server_ready(function(server)
+               local on_attach = function(_, bufnr)
+                  local function buf_set_keymap(...)
+                     vim.api.nvim_buf_set_keymap(bufnr, ...)
+                  end
+                  local function buf_set_option(...)
+                     vim.api.nvim_buf_set_option(bufnr, ...)
+                  end
+
+                  --Enable completion triggered by <c-x><c-o>
+                  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                  -- Mappings.
+                  local opts = { noremap = true, silent = true }
+                  -- See `:help vim.lsp.*` for documentation on any of the below functions
+                  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+                  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                  buf_set_keymap(
+                     'n',
+                     '<leader>D',
+                     '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+                     opts
+                  )
+                  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+                  buf_set_keymap('n', '<C-p>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+                  buf_set_keymap('n', '<C-n>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+                  buf_set_keymap(
+                     'n',
+                     '<space>Wa',
+                     '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+                     opts
+                  )
+                  buf_set_keymap(
+                     'n',
+                     '<space>Wr',
+                     '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+                     opts
+                  )
+                  buf_set_keymap(
+                     'n',
+                     '<space>Wl',
+                     '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                     opts
+                  )
+               end
+
+               local opts = {
+                  on_attach = on_attach,
+                  capabilities = capabilities,
+               }
+
+               if server == 'eslint' then
+                  opts.on_attach = function(client, bufnr)
+                     -- neovim's LSP client does not currently support dynamic capabilities registration, so we need to set
+                     -- the resolved capabilities of the eslint server ourselves!
+                     client.resolved_capabilities.document_formatting = true
+                     on_attach(client, bufnr)
+                  end
+                  opts.settings = {
+                     format = { enable = true }, -- this will enable formatting
+                  }
+               end
+
+               server:setup(opts)
+
+               vim.cmd [[ do User LspAttachBuffers ]]
+            end)
+         end,
       }
 
       use {
